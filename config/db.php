0d1d0fac2a69140756999f120775305c7a5532ed
youngxblood <crazyeight68@gmail.com>
2017-04-07 00:10:19 -0700

    Initial commit.

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii-financialreview',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
---
+++ views/site/financial.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Financial';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        Here you can see your financial status.
+    </p>
+
+    <code><?= __FILE__ ?></code>
+</div>
